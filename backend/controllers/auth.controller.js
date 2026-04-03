71d43c7a76b89bb7a1e391d414aaff4bf4728a10
vishal gupta <vishalgupta120703@gmail.com>
2026-04-03 17:51:42 +0530

    authentication done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -131,3 +131,17 @@ export const login = async (req, res) => {
         return res.status(500).json({ msg: "Something went wrong" });
     }
 };
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+        });
+
+        return res.status(200).json({ msg: "Logout successful" });
+    } catch (err) {
+        return res.status(500).json({ msg: "Something went wrong" });
+    }
+};
--- backend/routers/auth.router.js
+++ backend/routers/auth.router.js
@@ -1,5 +1,6 @@
 import express from "express"
-import {getAuth,generateOtp,verifyOTP,register,login} from "../controllers/auth.controller.js"
+import {getAuth,generateOtp,verifyOTP,register,login,logout} from "../controllers/auth.controller.js"
+import { isAuthenticated } from "../middleware/auth.middleware.js";
 const router = express.Router()
 
 router.get("/",getAuth);
@@ -7,5 +8,6 @@ router.post("/send-otp",generateOtp);
 router.post("/verify-otp",verifyOTP);
 router.post("/register",register);
 router.post("/login",login);
+router.post("/logout",isAuthenticated,logout);
 
 export default router;
